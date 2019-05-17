a761b1ee6a95461855847625ed059d48df69d63c
Humhunter <61627515@qq.com>
2019-05-17 12:46:42 +0800

    add deque

--- chapter1/_deque.py
+++ chapter1/_deque.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 # author: JinSong.xiao(61627515@qq.com)
 # time: 2019/5/15 19:12
+'''
+保留最后N个元素
+
+'''
 
 from collections import deque
 
@@ -13,6 +17,30 @@ def search(lines, patterns, history=5):
             yield  li, previous_lines
         previous_lines.append(li)
 
+def get_last_num_element(n):
+    q = deque(maxlen=n)
+    q.append(1)
+    q.append(2)
+    q.append(3)
+    print(q)
+    q.append(4)
+    q.append(5)
+    print(q)
+
+def deque_pop():
+    q = deque()
+    q.append(1)
+    q.append(2)
+    q.append(3)
+    print(q)
+    q.appendleft(4)
+    print(q)
+    q.pop()
+    print(q)
+    q.popleft()
+    print(q)
+
 
 if __name__ == '__main__':
-    
+    get_last_num_element(3)
+    deque_pop()
