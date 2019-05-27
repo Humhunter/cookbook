7eb0a854994b639a02a96841b83fbe7fbefbb741
Humhunter <61627515@qq.com>
2019-05-27 23:22:27 +0800

    add dict

--- chapter1/_dict.py
+++ chapter1/_dict.py
@@ -5,4 +5,28 @@
 
 
 from collections import defaultdict
+from collections import OrderedDict
 
+
+def set_multidict():
+    d = defaultdict(list)
+    d['a'].append(1)
+    d['a'].append(2)
+    d['b'].append(4)
+
+    e = defaultdict(set)
+    e['a'].add(1)
+    e['a'].add(2)
+    e['b'].add(4)
+    print(type(d), d, type(e), e)
+
+def set_ordered_dict():
+    d = OrderedDict()
+    d['foo'] = 1
+    d['bar'] = 2
+    d['spam'] = 4
+    d['grok'] = 3
+
+
+if __name__ == '__main__':
+    set_multidict()
